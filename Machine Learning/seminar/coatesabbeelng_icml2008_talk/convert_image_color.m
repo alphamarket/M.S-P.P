9e03463ec5d0fc8541e7b6b100b30137c7497f7f
dariushha <b.g.dariush+repo@gmail.com>
2014-12-29 23:51:07 +0330

    @ Machine Learning/seminar/convert_image_color.m: a fix

--- Machine Learning/seminar/coatesabbeelng_icml2008_talk/convert_image_color.m
+++ Machine Learning/seminar/coatesabbeelng_icml2008_talk/convert_image_color.m
@@ -1,8 +1,9 @@
 I = imread('C:\users\dariush\desktop\Untitled.png');
-c = [1 1 1].*255;%[0.9333,0.9529,0.9765].* 255;
+%c = [1 1 1].*255;
+c = [0.9333,0.9529,0.9765].* 255;
 for i=1:size(I,1)
     for j=1:size(I,2)
-        if mean(I(i,j,:)./255) >= 0.9
+        if mean(I(i,j)./255) >= 0.9
             I(i,j,:) = c;
         end
     end
